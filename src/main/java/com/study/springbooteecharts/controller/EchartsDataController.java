9bf2f0450ccc0a9a779eeed85695063b98ec70e0
Memo <2415782224@qq.com>
2023-05-19 13:12:30 +0800

    ajax请求的数据没有被渲染出来

--- src/main/java/com/study/springbooteecharts/controller/EchartsDataController.java
+++ src/main/java/com/study/springbooteecharts/controller/EchartsDataController.java
@@ -5,6 +5,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 /**
  * @author: Rrow
  * @date: 2023/5/19 12:13
@@ -20,8 +23,18 @@ public class EchartsDataController {
 
     @GetMapping("getData")
     @ResponseBody
-    public String getData() {
-        return "{'name':'猫羽雫'}";
+    public Object getData() {
+        HashMap<String, Object> hashMap = new HashMap<>();
+        ArrayList<String> list = new ArrayList<>();
+        list.add("123");
+        list.add("456");
+        list.add("444");
+        list.add("111");
+        list.add("555");
+        hashMap.put("data1",list);
+
+        // return "{'name':'猫羽雫'}";
+        return hashMap;
     }
 
 
